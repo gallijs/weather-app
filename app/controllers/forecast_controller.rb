655fbd53076e24e7af726a95676743ad001bf14d
Juan Gallisa <j.s.gallisa@gmail.com>
2023-01-06 16:05:36 -0400

    Cache the forecast details for 30 minutes using redis
    - Add test for the cached response

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -1,11 +1,22 @@
 class ForecastController < ApplicationController
   def index
     begin
+      $redis = Redis.new
+
+      # Return the cached result
+      if $redis.exists?(params_key)
+        cached = JSON.parse($redis.get(params_key))
+        return render json: { forecast: cached, cached: true }
+      end
+
       @forecast = WeatherAPI.fetch(
         forecast_params.fetch(:location, nil),
         forecast_params.fetch(:units, 'imperial'))
 
-      return render json: { forecast: @forecast }
+      # Create cache and store for 30 min
+      $redis.set(params_key, @forecast.to_json, ex: 1800)
+
+      return render json: { forecast: @forecast, cached: false }
     rescue => e
       logger.error e
       return render json: { message: "An error occurred while fetching the forecast." }, status: :unprocessable_entity
@@ -13,6 +24,11 @@ class ForecastController < ApplicationController
   end
 
   private
+
+  # Create unique key from the params
+  def params_key
+    forecast_params.to_json
+  end
 
   def forecast_params
     params.require(:forecast).permit(:location, :units);
--- spec/requests/forecast_spec.rb
+++ spec/requests/forecast_spec.rb
@@ -32,5 +32,30 @@ RSpec.describe 'Forecasts', type: :request do
         expect(forecast.temperature_unit).to eq('metric')
       end
     end
+
+    context 'when the response is cached' do
+      before(:each) do
+        # Initialize Redis this removes keys that might be present and interfere with tests
+        $redis = Redis.new
+        $redis.del({"location":"Toronto","units":"metric"}.to_json)
+      end
+
+      it 'calls the WeatherAPI once' do
+        allow(WeatherAPI).to(receive(:fetch)).and_call_original
+
+        get '/forecast', params: { forecast: { location: 'Toronto', units: 'metric' }}
+        expect(JSON.parse(response.body)['cached']).to eq(false)
+
+        get '/forecast', params: { forecast: { location: 'Toronto', units: 'metric' }}
+        expect(JSON.parse(response.body)['cached']).to eq(true)
+
+        expect(WeatherAPI).to have_received(:fetch).once
+
+        expect(response).to have_http_status(:success)
+        expect(forecast.city.name).to eq("Toronto")
+        expect(forecast.weather_conditions.length).to eq(7)
+        expect(forecast.temperature_unit).to eq('metric')
+      end
+    end
   end
 end
